62666ec429730a327aa9844b313d51c102f83da7
MRiley <44894070+helloriley@users.noreply.github.com>
2026-02-10 15:08:18 +0200

    Adding new project esgmatrix (#5)

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -25,9 +25,9 @@ export function Footer() {
             </Typography>
             <Typography
               color="blue-gray"
-              className="text-base font-medium text-blue-gray-300"
+              className="text-sm font-normal text-blue-gray-300"
             >
-              Made with React, <a href="https://astro.build/" target="_blank" className="text-blue-gray-700 hover:underline">Astro</a>, Tailwind CSS, and
+              Made with <a href="https://astro.build/" target="_blank" className="text-blue-gray-700 hover:underline">Astro</a>, React, Tailwind CSS, and
               <span className="h-5 w-5 inline-block align-top mx-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                 </svg>
