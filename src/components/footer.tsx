5e110d4dff6eca75a35916c2aeaf9651be29ee05
MRiley <44894070+helloriley@users.noreply.github.com>
2026-02-17 15:43:23 +0200

    update esgmatrix photos and a few sunexchange concepts (#8)

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -58,7 +58,7 @@ export function Footer() {
             className="text-sm font-normal text-blue-gray-300 mt-2"
           >
             Made with 
-            <span className="h-5 w-5 inline-block align-top mx-1"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth={1.5} className="size-5 fill-accent-300/80 stroke-blue-gray">
+            <span className="h-5 w-5 inline-block align-top mx-1"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth={1.5} className="size-5 fill-blue-gray-200 stroke-blue-gray">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
               </svg>
             </span>
